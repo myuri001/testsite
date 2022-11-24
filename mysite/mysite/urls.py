c331007373cdebb42b7ef24fa3f10fa9e5950ff9
myuri001 <63941768+myuri001@users.noreply.github.com>
2022-11-25 00:42:18 +0600

    Создал еще одну страницу test

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from news.views import index, test
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('news/', index),
+    path('test/', test)
 ]
